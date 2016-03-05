5819e9bf786b16119fbc48592783a834f089b7a8
Evangelos Mamalakis <mamalakis@auth.gr>
2016-03-05 12:27:23 +0200

    Implement Site fronted

---
+++ CodeBankSite/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+
+<title>CodeBank</title>
+<style >
+    body{
+         background-image: url("static/back.jpg");
+        /*background-color: #cccccc;*/
+    }
+    #panelLogin{
+        background-color: #cccccc;
+        margin-top: 4em;
+    }
+
+    #iconWearable{
+        width: 6em;
+        height: 6em;
+        margin-left: 0em;
+    }
+</style>
+
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4" id="panelLogin">
+            <h1 class="text-center login-title">CodeBank</h1>
---
+++ CodeBankSite/index.php
@@ -0,0 +1,24 @@
+<?php include 'header.php' ?>
+            <div class="account-wall">
+                <br>
+                <br>
+                <br>
+                <form method="POST" class="form-signin" action="ReadArduinoTextWithPhp.php">
+                <input class="form-control" id="username" name="username" placeholder="username" required autofocus>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit" id="signIn">
+                    Sign in</button>
+               
+                <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+                </form>
+            </div>
+            <a href="#" class="text-center new-account">Create an account </a>
+             <br>
+             <h2>
+            <p>Οr sign in with codeBank wearable</p>
+             </h2>
+              <div class="row">
+                    <div class="col-sm-6 col-md-4 col-md-offset-4" >
+                        <img src="static/wearable.jpg" id="iconWearable" />
+                    </div>
+        </div>
